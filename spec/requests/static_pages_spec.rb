e8e72f7eec9ae8a13faae52ef88473fb4d3eadf4
Anthony Brummett <abrummet@genome.wustl.edu>
2012-08-17 18:46:45 -0500

    Refactor test to be more succinct

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,61 +3,34 @@ require 'spec_helper'
 describe "Static Pages" do
 
     let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+    subject { page }
 
     describe "Home page" do
-        it "should have the content 'Sample App'" do
-            visit root_path
-            page.should have_selector('h1', text: 'Sample App')
-        end
+        before { visit root_path }
 
-        it "should have the base title" do
-            visit root_path
-            page.should have_selector('title', text: base_title)
-        end
-
-        it "should not have a custom title syffix" do
-            visit root_path
-            page.should_not have_selector('title',
-                                        text: "#{base_title} | Home")
-        end
+        it { should have_selector('h1', text: 'Sample App') }
+        it { should have_selector('title', text: full_title_with_suffix('')) }
+        it { should_not have_selector('title', text: full_title_with_suffix('Home')) }
     end
 
     describe "Help page" do
-        it "should have the content 'Help'" do
-            visit help_path
-            page.should have_selector('h1', text: 'Help');
-        end
+        before { visit help_path }
 
-        it "should have the right title" do
-            visit help_path
-            page.should have_selector('title',
-                                        text: "#{base_title} | Help")
-        end
+        it { should have_selector('h1', text: 'Help') }
+        it { should have_selector('title', text: full_title_with_suffix('Help')) }
     end
 
     describe "About page" do
-        it "should have the content 'About Us'" do
-            visit about_path
-            page.should have_selector('h1', text: 'About Us')
-        end
+        before { visit about_path }
 
-        it "should have the right title" do
-            visit about_path
-            page.should have_selector('title',
-                                        text: "#{base_title} | About Us")
-        end
+        it { should have_selector('h1', text: 'About Us') }
+        it { should have_selector('title', text: full_title_with_suffix('About Us')) }
     end
 
     describe "Contact page" do
-        it "should have the content 'Contact Us'" do
-            visit contact_path
-            page.should have_selector('h1', text: 'Contact Us')
-        end
+        before { visit contact_path }
 
-        it "should have the right title" do
-            visit contact_path
-            page.should have_selector('title',
-                                        text: "#{base_title} | Contact Us")
-        end
+        it { should have_selector('h1', text: 'Contact Us') }
+        it { should have_selector('title', text: full_title_with_suffix("Contact Us")) }
     end
 end
